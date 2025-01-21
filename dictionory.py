f9020e65d5dcc24ed4222410ada3f55c3bba6c54
unknown <sandeshgaikwad1994@gmail.com>
2025-01-21 10:00:57 +0530

    WIP: This is fist file commoted by sandesh, directory chnages added

---
+++ dictionory.py
@@ -0,0 +1,24 @@
+# dictionary --> unordered, key-value pair,
+
+mydic = {
+    "name": "sandesh",
+    "age": 29,
+    "bmi": 20.5
+} 
+print(mydic["age"])
+
+# update the value of key
+mydic = {
+    "name": "sandesh",
+    "age": 29,
+    "bmi": 20.5
+} 
+mydic["age"] = 31
+print(mydic["age"])
+
+
+# for loop 
+for i in mydic:
+    print(i, mydic[i])
+
+# Filter the value , slipt,
